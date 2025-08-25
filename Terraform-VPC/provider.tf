14fa65e27fe57b8e9d2cef9a78302946f767661e
Rishmita <rishmita1303@gmail.com>
2025-08-25 15:59:53 +0530

    Update provider.tf

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "terraform-remote-backend--s3"
+    bucket = "terraform-remote-backend-s3"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
